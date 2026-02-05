b0551320f7ea5b5c0cb094e1b6fe9cf205e1ceca
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-02-05 02:55:56 -0500

    update

--- Mathlib/MeasureTheory/Function/StronglyMeasurable/Basic.lean
+++ Mathlib/MeasureTheory/Function/StronglyMeasurable/Basic.lean
@@ -416,8 +416,8 @@ protected theorem inv [Inv β] [ContinuousInv β] (hf : StronglyMeasurable f) :
     StronglyMeasurable f⁻¹ :=
   ⟨fun n => (hf.approx n)⁻¹, fun x => (hf.tendsto_approx x).inv⟩
 
-@[to_additive (attr := fun_prop)]
-protected theorem div [Div β] [ContinuousDiv β] (hf : StronglyMeasurable f)
+@[to_additive (attr := fun_prop) sub]
+protected theorem div' [Div β] [ContinuousDiv β] (hf : StronglyMeasurable f)
     (hg : StronglyMeasurable g) : StronglyMeasurable (f / g) :=
   ⟨fun n => hf.approx n / hg.approx n, fun x => (hf.tendsto_approx x).div' (hg.tendsto_approx x)⟩
 
@@ -426,7 +426,7 @@ theorem div₀ [GroupWithZero β] [ContinuousMul β] [ContinuousInv₀ β] (hf :
   ⟨fun n => hf.approx n / hg.approx n,
     fun x => (hf.tendsto_approx x).div (hg.tendsto_approx x) (h₀ x)⟩
 
-theorem div₀' [GroupWithZero β] [ContinuousMul β] [ContinuousInv₀ β] [PseudoMetrizableSpace β]
+theorem div [GroupWithZero β] [ContinuousMul β] [ContinuousInv₀ β] [PseudoMetrizableSpace β]
     [MeasurableSpace β] [BorelSpace β] [MeasurableSingletonClass β] (hf : StronglyMeasurable f)
     (hg : StronglyMeasurable g) :
     StronglyMeasurable (f / g) := by
