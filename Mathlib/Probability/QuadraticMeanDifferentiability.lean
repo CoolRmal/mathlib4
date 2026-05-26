ca5432de879d5c104698b14381889a34328d0a06
Rmal <97214596+CoolRmal@users.noreply.github.com>
2026-05-26 13:08:36 -0400

    Update QuadraticMeanDifferentiability.lean

--- Mathlib/Probability/QuadraticMeanDifferentiability.lean
+++ Mathlib/Probability/QuadraticMeanDifferentiability.lean
@@ -6,6 +6,7 @@ Authors: Yongxi Lin
 module
 
 public import Mathlib.Analysis.Asymptotics.TVS
+public import Mathlib.MeasureTheory.Measure.Decomposition.IntegralRNDeriv
 public import Mathlib.MeasureTheory.Measure.Decomposition.Lebesgue
 public import Mathlib.MeasureTheory.Function.LpSeminorm.Defs
 public import Mathlib.MeasureTheory.Function.LpSpace.Basic
@@ -25,17 +26,43 @@ open Filter MeasureTheory Real
 
 namespace ProbabilityTheory
 
-namespace QuadraticMeanDifferentiability
-
 def QuadraticMeanDifferentiableAt {Ω E : Type*} {mΩ : MeasurableSpace Ω} [NormedAddCommGroup E]
-    [NormedSpace ℝ E] (P : E → Measure Ω) (μ : Measure Ω) (θ : E) (s : Set E) : Prop :=
-  ∃ A : E →L[ℝ] (Ω →₂[P θ] ℝ), (fun h ↦ lpNorm
-    (fun ω => sqrt (μ.rnDeriv (P (θ + h)) ω).toReal -
-    sqrt (μ.rnDeriv (P θ) ω).toReal - 2⁻¹ * (A h ω) * sqrt (μ.rnDeriv (P θ) ω).toReal) 2 μ)
+    [NormedSpace ℝ E] (P : E → Measure Ω) (μ : Measure Ω) (θ : E) (s : Set E)
+    (A : E →L[ℝ] (Ω →₂[P θ] ℝ)) : Prop :=
+  (fun h => lpNorm
+    (fun ω => sqrt ((P (θ + h)).rnDeriv μ ω).toReal -
+    sqrt ((P θ).rnDeriv μ ω).toReal - 2⁻¹ * (A h ω) * sqrt ((P θ).rnDeriv μ ω).toReal) 2 μ)
     =o[𝓝[s] 0] norm
 
+variable {Ω E : Type*} {mΩ : MeasurableSpace Ω} [NormedAddCommGroup E] [NormedSpace ℝ E]
+  {P : E → Measure Ω} {μ : Measure Ω} {θ : E} {s : Set E} {A : E →L[ℝ] (Ω →₂[P θ] ℝ)}
 
+lemma lemma1 {m₁ m₂ : Measure Ω} [SigmaFinite μ] [IsProbabilityMeasure m₁]
+    [IsProbabilityMeasure m₂] (hm₁ : m₁ ≪ μ) (hm₂ : m₂ ≪ μ) :
+    ∫ ω, (sqrt (m₁.rnDeriv μ ω).toReal - sqrt (m₂.rnDeriv μ ω).toReal) *
+      (sqrt (m₁.rnDeriv μ ω).toReal + sqrt (m₂.rnDeriv μ ω).toReal) ∂μ = 0 := by
+  rw [integral_congr_ae, integral_sub (Measure.integrable_toReal_rnDeriv (μ := m₁) (ν := μ))
+      (Measure.integrable_toReal_rnDeriv (μ := m₂) (ν := μ)),
+      Measure.integral_toReal_rnDeriv hm₁, Measure.integral_toReal_rnDeriv hm₂]
+  · simp
+  · filter_upwards with ω
+    calc
+      _ = sqrt (m₁.rnDeriv μ ω).toReal ^ 2 - sqrt (m₂.rnDeriv μ ω).toReal ^ 2 := by ring
+      _ = (m₁.rnDeriv μ ω).toReal - (m₂.rnDeriv μ ω).toReal := by
+        simp [sq_sqrt ENNReal.toReal_nonneg]
 
-end QuadraticMeanDifferentiability
+theorem lim1 (hs : ∀ x ∈ s, P x ≪ μ) (h : E) :
+    Tendsto (fun t => ∫ ω,
+      (sqrt ((P (θ + t • h)).rnDeriv μ ω).toReal - sqrt ((P θ).rnDeriv μ ω).toReal -
+      2⁻¹ * A h ω * sqrt ((P θ).rnDeriv μ ω).toReal) *
+      (sqrt ((P (θ + t • h)).rnDeriv μ ω).toReal + sqrt ((P θ).rnDeriv μ ω).toReal) ∂μ) (𝓝[>] 0)
+      (𝓝 <| ∫ ω, A h ω * ((P θ).rnDeriv μ ω).toReal ∂μ) := by
+  sorry
+
+theorem lim2 (hs : ∀ x ∈ s, P x ≪ μ) (h : E) :
+    Tendsto (fun t => ∫ ω, A h ω * sqrt ((P θ).rnDeriv μ ω).toReal *
+      (sqrt ((P (θ + t • h)).rnDeriv μ ω).toReal + sqrt ((P θ).rnDeriv μ ω).toReal) ∂μ) (𝓝[>] 0)
+      (𝓝 <| ∫ ω, A h ω * ((P θ).rnDeriv μ ω).toReal ∂μ) := by
+  sorry
 
 end ProbabilityTheory
